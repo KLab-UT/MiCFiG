43daa6bc27ccf636dc155cdd5aad1415c08ff10c
KLab-UT <utt.klab@gmail.com>
2023-09-06 13:57:26 -0600

    created docstrings for purpose of script and sys arguments for gff_mito_scan.py

--- gff_mito_scan.py
+++ gff_mito_scan.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+'''
+This script is to search a gff file (a genome annotation file) for nuclear-encoded, mitochondrial-targeting gens (Nmt genes)
+sys allows the user to provide arguments from the command line
+sys.argv is a list of the arguments.
+sys.argv[0] is the name of the python script.
+sys.argv[1] should be the name of the Human_MitoCarta3.0.csv file
+sys.argv[2] should be the name of the gff file being read in
+sys.argv[3] should be the name of the gff file being written out
+'''
 
 import sys
 
