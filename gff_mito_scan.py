8ea3672fb3db10fe6e6bcb75cd8a94c393151d56
Erin R. Bunch <erinbunch2801@live.com>
2023-09-27 10:36:59 -0600

    Fixed the list errors; now using four files insead of three to run the code; Printed the new mito lists to 'search_tearm.txt' which is the new addittional file; starting to work on parsing through the gff file using the mito lists

--- gff_mito_scan.py
+++ gff_mito_scan.py
@@ -16,7 +16,8 @@ import sys
 if __name__ == "__main__":
     mito_carta_file = open(sys.argv[1], 'r')
     gff_file_in = open(sys.argv[2], 'r')
-    gff_file_out = open(sys.argv[3], 'w')
+    mito_file_out = open(sys.argv[3], 'w')
+    gff_file_out = open(sys.argv[4], 'w')
     # for each line in mito_carta_file, create search criteria
     mito_carta_file.readline()
     for line in mito_carta_file:
@@ -27,13 +28,20 @@ if __name__ == "__main__":
         synonyms = line[1].split('|')
         description = line[2].strip()
         search_terms = synonyms
-        search_terms = search_terms.append(human_symbol)
-        print(search_terms)
-        search_terms = search_terms.append(description)
+        search_terms.append(human_symbol)
+        search_terms.append(description)
+        print(search_terms, file=mito_file_out)
+        #^splits up all of the terms in the csv file and appends them to a list and then prints them
+        #to a file, they are all separate lists and some contain nothing
         # for each line in gff_file_in, search for gene names
-        lines_with_search_terms = [gff_line for gff_line in gff_file_in if any(term in gff_line for term in search_terms)]
-        print(lines_with_search_terms)
+   # print(search_terms) >> gff_file_out
+    for gff_line in gff_file_in:
+        if any term in gff_line for term in mito_list in mito_file_out:
+            #for mito_list in mito_file_out
+            #lines_with_search_terms = [gff_line for gff_line in gff_file_in if any(term in gff_line for term in mito_list)]
+            print(lines_with_search_terms, file=gff_file_out)
         # if search criteria matches gff_file_in line, write this line to gff_file_out
     mito_carta_file.close()
     gff_file_in.close()
+    mito_file_out.close()
     gff_file_out.close()
