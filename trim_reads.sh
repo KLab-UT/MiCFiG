8702b0b8da179d1b1d8e15f678cab0d816f9ea5a
Dante Celani <u6049734@lp072.lonepeak.peaks>
2023-10-05 16:35:29 -0600

    trimmomatic in trim_reads.sh having issues

--- trim_reads.sh
+++ trim_reads.sh
@@ -2,13 +2,15 @@
 
 # PE [-version] [-threads <threads>] [-phred33|-phred64] [-trimlog <trimLogFile>] [-summary <statsSummaryFile>] [-quiet] [-validatePairs] [-basein <inputBase> | <inputFile1> <inputFile2>] [-baseout <outputBase> | <outputFile1P> <outputFile1U> <outputFile2P> <outputFile2U>] <trimmer1>...
 
-wd=/scratch/general/nfs1/utu_4310/whiptail_nmt_variation_data/
+wd=/scratch/general/nfs1/utu_4310/whiptail_nmt_variation_data
 mkdir -p $wd/trimmed_reads
 mkdir -p $wd/trimmed_reads/paired_reads
 mkdir -p $wd/trimmed_reads/unpaired_reads
 
+module load trimmomatic
+
 cd $wd/raw_reads
-ls *1.fq.gz | cut -d "_" -f "1,2,3,4" | sort | uniq > fq_list.txt
+ls *1.fq.gz | cut -d "_" -f 1,2,3,4 | sort | uniq > fq_list.txt
 while read sample; do
 	read1=$wd/raw_reads/${sample}_1.fq.gz 
 	read2=$wd/raw_reads/${sample}_2.fq.gz
@@ -16,6 +18,8 @@ while read sample; do
 	output_unpaired_1=$wd/trimmed_reads/unpaired_reads/${sample}_unpaired_1.fq.gz
 	output_paired_2=$wd/trimmed_reads/paired_reads/${sample}_paired_2.fq.gz
 	output_unpaired_2=$wd/trimmed_reads/unpaired_reads/${sample}_unpaired_2.fq.gz
-	java -jar trimmomatic/0.39 PE -version trimmomatic-0.39 -threads 8 -trimlog trimlog.txt -summary summary.txt -basein $read1 $read2 -baseout $output_paired_1 $output_unpaired_1 $output_paired_2 $output_unpaired_2
+	trimmomatic PE -threads 8 -trimlog trimlog.txt -summary summary.txt -basein $read1 $read2 -baseout $output_paired_1 $output_unpaired_1 $output_paired_2 $output_unpaired_2
 
 done<fq_list.txt
+
+module unload trimmomatic
