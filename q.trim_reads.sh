0bfd3725d882a9549e95df60bb1f0a386360b7a3
Dante Celani <u6049734@lonepeak1.wasatch.peaks>
2023-10-07 02:47:31 -0600

    added slurm* to .gitignore and made q.trim_reads.sh to trim reads.

--- q.trim_reads.sh
+++ q.trim_reads.sh
@@ -16,6 +16,7 @@ mkdir -p $wd/trimmed_reads/paired_reads
 mkdir -p $wd/trimmed_reads/unpaired_reads
 
 module load trimmomatic
+#module load fastp/0.20.1
 
 cd $wd/raw_reads
 ls *1.fq.gz | cut -d "_" -f 1,2,3,4 | sort | uniq > fq_list.txt
@@ -26,10 +27,16 @@ while read sample; do
 	output_u_1=$wd/trimmed_reads/unpaired_reads/${sample}_unpaired_1.fq.gz
 	output_p_2=$wd/trimmed_reads/paired_reads/${sample}_paired_2.fq.gz
 	output_u_2=$wd/trimmed_reads/unpaired_reads/${sample}_unpaired_2.fq.gz
-	trimmomatic PE $read1 $read2 $output_p_1 $output_u_1 $output_p_2 $output_u_2 
-	#ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True
-	#trimmomatic PE -threads 8 -trimlog trimlog.txt -summary summary.txt -basein $read1 $read2 -baseout $output_paired_1 $output_unpaired_1 $output_paired_2 $output_unpaired_2
-
+	trimmomatic PE $read1 $read2 $output_p_1 $output_u_1 $output_p_2 $output_u_2 SLIDINGWINDOW:5:20 
+	#MINLEN:50
+	#trimmomatic PE -threads 16 -trimlog trimlog.txt -summary summary.txt -basein $read1 $read2 -baseout $output_paired_1 $output_unpaired_1 $output_paired_2 $output_unpaired_2
+#	fastp 	-w 16 \
+#		-i $read1 -I $read2 \
+#		-m --merged_out $output_p_1 / --out1 $output_u_1 --out2 $output_u_2 \
+#	gzip $output_p_1
+#	gzip $output_u_1
+#	gzip $output_u_2 
 done<fq_list.txt
 
 module unload trimmomatic
+#module load fastp/0.20.1
