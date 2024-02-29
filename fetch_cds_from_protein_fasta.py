59874aa1486f705116e560b98af90ae1f9cf1dfb
reaganman <reaganman@github.com>
2024-02-29 12:46:22 -0700

    update script to fetch seqs

--- fetch_cds_from_protein_fasta.py
+++ fetch_cds_from_protein_fasta.py
@@ -1,19 +1,30 @@
 import argparse
 import os 
 from Bio import SeqIO, Entrez
+import time 
 
-def fetch_protein_cds(protein_accession):
+def fetch_protein_cds(protein_accession, output_directory, max_retries=3, retry_delay=1):
     """
     Fetch the coding sequences for the provided protein accession using NCBI Entrez
     """
 
-    # Fetch the protein record using the protein accession ID
-    try:
-        handle = Entrez.efetch(db="protein", id=protein_accession, rettype="gb", retmode="text")
-        protein_record = SeqIO.read(handle, "genbank")
-        #print(f"Protein Record: {protein_record}")
-    except Exception as e:
-        print(f"Error fetching protein record: {e}")
+    retry_count = 0
+    while retry_count < max_retries:
+        try:
+            handle = Entrez.efetch(db="protein", id=protein_accession, rettype="gb", retmode="text")
+            protein_record = SeqIO.read(handle, "genbank")
+            handle.close()
+            print(f"Successfully fetched protein record for {protein_accession}")
+            break  # If successful, exit the loop
+        except Exception as e:
+            print(f"Error fetching protein record (Attempt {retry_count + 1}): {e}")
+            retry_count += 1
+            time.sleep(retry_delay)
+
+    if retry_count == max_retries:
+        print(f"Max retries reached for {protein_accession}, appending to problem_seqs.")
+        with open("problem_seqs", "a") as problem_seqs:
+            problem_seqs.write(f"{protein_accession}\n")
         return None
 
     # Get accession ID encoding sequence associated with protein accession
@@ -25,22 +36,33 @@ def fetch_protein_cds(protein_accession):
             #print(f"Coding accesion: {coding_accession}")
 
             #fetch the encoding sequence
-            try:
-                coding_handle = Entrez.efetch(db="nuccore", id=coding_accession, rettype="fasta", retmode="text")
-                coding_record = SeqIO.read(coding_handle, "fasta")
-                coding_handle.close()
+            retry_count = 0
+            while retry_count < max_retries:
+                try:
+                    coding_handle = Entrez.efetch(db="nuccore", id=coding_accession, rettype="fasta", retmode="text")
+                    coding_record = SeqIO.read(coding_handle, "fasta")
+                    coding_handle.close()
 
-                # nucleotides = {"A", "T", "G", "C"}
-                #sequence = str(coding_record.seq)
+                    # nucleotides = {"A", "T", "G", "C"}
+                    #sequence = str(coding_record.seq)
 
-                # # Replace characters not in nucleotides or '-' with '-'
-                # sanitized_sequence = ''.join(c if c in nucleotides or c == '-' else '-' for c in sequence)
+                    # # Replace characters not in nucleotides or '-' with '-'
+                    # sanitized_sequence = ''.join(c if c in nucleotides or c == '-' else '-' for c in sequence)
 
-                return coding_record
+                    # Write sequence
+                    output_file_path = os.path.join(output_directory, f"{coding_record.id}.fasta")
+                    SeqIO.write(coding_record, output_file_path, 'fasta')
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
+                except Exception as e:
+                    print(f"Error fetching protein record (Attempt {retry_count + 1}): {e}")
+                    retry_count += 1
+                    time.sleep(retry_delay)
+            if retry_count == max_retries:
+                print(f"Max retries reached for {protein_accession}, appending to problem_seqs.")
+                with open("problem_seqs", "a") as problem_seqs:
+                    problem_seqs.write(f"{protein_accession}\n")
                 return None
+
         
 def get_accesions(fasta_file):
     """
@@ -51,7 +73,6 @@ def get_accesions(fasta_file):
 
     for record in records:
         accessions.add(record.id)
-
     return accessions
 
 
@@ -64,8 +85,6 @@ def save_records(records, output_directory):
         with open(output_file_path, 'w') as output_handle:
             SeqIO.write(record, output_handle, 'fasta')
         
-
-
 
 
 def main():
@@ -87,14 +106,7 @@ def main():
 
     # Get CDS from CDS accessions
     for accession in protein_accessions:
-        cds_record = fetch_protein_cds(accession)
-        #print(f"Record ID: {cds_record.id}\nRecord Description: {cds_record.description}\nRecord seq: {cds_record.seq}")
-        cds_records.append(cds_record)
-
-    # Save CDS as individual fastas
-    save_records(cds_records, fastas_dir)
-
-    
+        fetch_protein_cds(accession, fastas_dir)
 
 
 
