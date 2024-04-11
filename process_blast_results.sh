b8341dee8973009bedc7a4599045c88d560e7b60
David-Bean <david.w.bean98@gmail.com>
2024-04-11 13:52:07 -0600

    started script to parse blast results

--- process_blast_results.sh
+++ process_blast_results.sh
@@ -3,7 +3,14 @@
 # end
 # Creates output file for grabbing annotations with range overlap from gff
 #   Ouput = Query ID / Chromosome / Start / Stop
-results_dir=$1
+
+
+#Results directory: /scratch/general/nfs1/utu_4310/whiptail_shared_data/blastn_results
+
+# Results dir =
+results_dir=./test_results_dir
 output_file=$2
+
+ls $results_dir
 
 # Parse the results files in results dir
