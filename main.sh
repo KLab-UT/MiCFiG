e30974be70afdfe900f8be168c6168c673d39353
David Bean <david98bean@gmail.com>
2024-04-11 11:13:58 -0600

    I can't pullll

--- main.sh
+++ main.sh
@@ -1,2 +1,10 @@
+#What he had on the white board
+workingDir=/scratch/general/nfs1/utu_4310/whiptail_shared_data
+outputDir=/scratch/general/nfs1/utu_4310/whiptail_shared_data/blast_results
 
+ls $workingDir
+#
+#
+#
+#
 
--- process_blast_results.sh
+++ process_blast_results.sh
@@ -2,8 +2,12 @@
 # For each chromosome hit, grab the minimum subject start and maximum subject
 # end
 # Creates output file for grabbing annotations with range overlap from gff
-#   Ouput = Query ID / Chromosome / Start / Stop
-results_dir=$1
-output_file=$2
+#   Ouput = Query ID , Chromosome , Start , Stop
 
-# Parse the results files in results dir
+# Can start with the test files that regan made
+
+#Results file
+results_dir=$1 # sratch blast_results
+output_file=$2 # scratch processed_ ... file...
+
+# Parse the results files in results dir (blast_results in scratch)
