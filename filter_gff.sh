67cd5bf52fd9afcb8281c502311db6a5488888f9
Alexander Everett <u6052681@lonepeak2.wasatch.peaks>
2024-04-16 14:56:25 -0600

    Updated filter_gff.sh

--- filter_gff.sh
+++ filter_gff.sh
@@ -30,13 +30,20 @@ overlap() {
     [ "$blast_start" -le "$gff_stop" ] && [ "$blast_stop" -ge "$gff_start" ]
 }
 
-while IFS=$"\t" read -r chromosome start_value stop_value; do
-    echo($chromsome)
-    echo($start_value)
-    echo($stop_value)
-#    awk -v blast_start="$start_value" -v blast_stop="$stop_value" -v blast_chromo="$chromosome" \
-#        {if ($1 == blast_chromo && overlap(blast_start, blast_stop, $4, $5)) print} "$input_gff"
-done < "$input_bed" > "$output_gff"
+cat $input_bed
 
+awk '{overlap($1, $2, $3)}' $input_bed
+
+#awk -v blast_start="$start_value" -v blast_stop="$stop_value" -v blast_chromo="$chromosome" \
+#    {if ($1 == blast_chromo && overlap(blast_start, blast_stop, $4, $5)) print} "$input_gff"
+#
+#while IFS=$"\t" read -r chromosome start_value stop_value; do
+#    echo $chromsome
+#    echo $start_value
+#    echo $stop_value
+##    awk -v blast_start="$start_value" -v blast_stop="$stop_value" -v blast_chromo="$chromosome" \
+##        {if ($1 == blast_chromo && overlap(blast_start, blast_stop, $4, $5)) print} "$input_gff"
+#done < "$input_bed" > "$output_gff"
+#
 # I'm assuming that the BED file is simply chromosome number, start coordinate,
 # and stop coordinate.
