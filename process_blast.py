c34a1087dc006b32b909a290fe15ba8248f28524
Reagan Mckee <u6052680@lp109.lonepeak.peaks>
2024-04-24 10:22:45 -0600

    update scripts

---
+++ process_blast.py
@@ -0,0 +1,186 @@
+#
+#
+# process_blast.py 
+# script to process xml blast results and make a bed file for the top hits, also creates a csv for query_gene, pident
+# 
+# 
+import argparse, os
+import xml.etree.ElementTree as ET
+from Bio import Entrez
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from collections import Counter
+import re
+import csv
+
+class BlastHit:
+    """
+    Manage blast hits from ET element
+    """
+    def __init__(self, hit_element, query_gene, query_id, query_length):
+        self.query_gene = query_gene
+        self.query_id = query_id
+        self.query_len = int(query_length)
+        self.accession = hit_element.find("Hit_accession").text
+        self.definition = hit_element.find("Hit_def").text
+        self.length = int(hit_element.find("Hit_len").text)
+
+        print(f"Hit number: {(hit_element.find('Hit_num').text)}")
+        self.hit_num = int((hit_element.find('Hit_num').text))
+        
+
+        # Iterate over Hsp elements and create Hsp objects if #1 hit
+        self.top_hsp = None
+        if self.hit_num == 1: 
+            for hsp_element in hit_element.findall("Hit_hsps/Hsp"):
+                hsp = self.create_hsp_object(hsp_element)
+                if hsp.hsp_num == 1:
+                    self.top_hsp=hsp
+                    break
+        self.overall_identity = self.calculate_overall_identity() # NOT ACTUALLY OVERALL IDEntity
+        
+    
+    def create_hsp_object(self, hsp_element):
+        """
+        Create Hsp object for blast hits from Hsp XML element
+        """
+        return Hsp(
+            hsp_num=int(hsp_element.find("Hsp_num").text),
+            bit_score=float(hsp_element.find("Hsp_bit-score").text),
+            score=int(hsp_element.find("Hsp_score").text),
+            evalue=float(hsp_element.find("Hsp_evalue").text),
+            query_start=int(hsp_element.find("Hsp_query-from").text),
+            query_end=int(hsp_element.find("Hsp_query-to").text),
+            hit_start=int(hsp_element.find("Hsp_hit-from").text),
+            hit_end=int(hsp_element.find("Hsp_hit-to").text),
+            query_frame=int(hsp_element.find("Hsp_query-frame").text),
+            hit_frame=int(hsp_element.find("Hsp_hit-frame").text),
+            identity=int(hsp_element.find("Hsp_identity").text),
+            positive=int(hsp_element.find("Hsp_positive").text),
+            gaps=int(hsp_element.find("Hsp_gaps").text),
+            align_len=int(hsp_element.find("Hsp_align-len").text),
+            qseq=hsp_element.find("Hsp_qseq").text,
+            hseq=hsp_element.find("Hsp_hseq").text,
+            midline=hsp_element.find("Hsp_midline").text
+        )
+    
+    def calculate_overall_identity(self):
+        """
+        Calculates the overall percent idendity for a blast hit taking all hsps into account
+        Identity is determined by summing the identical characters for each = (Hsp_idnetity x align_len / 10)
+        and dividing this sum by total query length then round to nearest int
+        """
+        if self.top_hsp is not None:
+            return self.top_hsp.identity
+        return 0
+
+    def make_bed(self, output_path):
+        """
+        Make bed file for blast hit, with an entry for each hsp
+        """
+        with open(output_path, 'w') as bed_out:
+            bed_line = "\t".join([self.definition, str(self.top_hsp.hit_start), str(self.top_hsp.hit_end), self.query_id])
+            bed_out.write(bed_line)
+    
+    def get_query_gene(self, gene_ids_csv):
+        """
+        Uses the query id to find gene name in csv with genes and ids. Gene needs to be first field in csv w/ ids
+        """
+        with open(gene_ids_csv, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if self.query_id in row:
+                    return row[0]  # Return the first field of the row where the ID is found
+        return None  # Return None if the query ID is not found in the CSV
+
+
+
+class Hsp:
+    """
+    Manage HSP information
+    An HSP is a high scoring pair representing a specific aligngment within a sequence hit by blast
+    """
+    def __init__(self, hsp_num, bit_score, score, evalue, query_start, query_end,
+                 hit_start, hit_end, query_frame, hit_frame, identity,
+                 positive, gaps, align_len, qseq, hseq, midline):
+        self.hsp_num = hsp_num
+        self.bit_score = bit_score
+        self.score = score
+        self.evalue = evalue
+        self.query_start = query_start
+        self.query_end = query_end
+        self.hit_start = hit_start
+        self.hit_end = hit_end
+        self.query_frame = query_frame
+        self.hit_frame = hit_frame
+        self.identity = identity
+        self.positive = positive
+        self.gaps = gaps
+        self.align_len = align_len
+        self.qseq = qseq
+        self.hseq = hseq
+        self.midline = midline
+    
+
+def get_query_gene(query_id, gene_ids_csv):
+    """
+    Uses the query id to find gene name in csv with genes and ids. Gene needs to be first field in csv w/ ids
+    """
+    with open(gene_ids_csv, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if query_id in row:
+                return row[0]  # Return the first field of the row where the ID is found
+    return None  # Return None if the query ID is not found in the CSV
+
+def log_identity(blast_hit, identities_csv):
+    """
+    Make a csv containing the overall identity for the top Blast hit of a gene
+    """
+    entry = (blast_hit.query_gene, blast_hit.overall_identity)
+    # Append the entry to the CSV file
+    with open(identities_csv, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(entry)
+
+
+
+def parse_blast(tree_root, gene_ids_csv, bed_output_path, identities_output_csv):
+    """
+    """
+    query_id = tree_root.find(".//Iteration_query-def").text
+    query_gene_name = get_query_gene(query_id, gene_ids_csv)
+    query_length = tree_root.find(".//Iteration_query-len").text
+
+    #Process blast hits
+    top_hit = None
+    for hit_element in tree_root.findall(".//Hit"):
+        hit = BlastHit(hit_element, query_gene_name, query_id, query_length)
+        if hit.hit_num == 1:
+            top_hit = hit
+            top_hit.make_bed(bed_output_path)
+            log_identity(top_hit, identities_output_csv)
+            break
+    
+
+def main(xml_file, gene_ids_csv, bed_output_path, identities_output_csv):
+    # Read XML blast results 
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    # Process blast hits
+    parse_blast(root, gene_ids_csv, bed_output_path, identities_output_csv)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process XML blast results and create a BED file for the top hits, also creates a CSV for query_gene, pident")
+    parser.add_argument("xml_file", help="Path to the XML file containing blast results")
+    parser.add_argument("gene_ids_csv", help="Path to the CSV file containing gene IDs")
+    parser.add_argument("--bed_output_path", default=".", help="path to save the BED files (default: current directory)")
+    parser.add_argument("--identities_output_csv", default="identities.csv", help="Path to the output CSV file for gene identities (default: identities.csv)")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args.xml_file, args.gene_ids_csv, args.bed_output_path, args.identities_output_csv)
---
+++ process_gffs.py
@@ -0,0 +1,83 @@
+import argparse
+import os
+import csv
+
+def get_query_gene(query_id, gene_ids_csv):
+    """
+    Uses the query id to find gene name in csv with genes and ids. Gene needs to be first field in csv w/ ids
+    """
+    with open(gene_ids_csv, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if query_id in row:
+                return row[0]  # Return the first field of the row where the ID is found
+    return None  # Return None if the query ID is not found in the CSV
+
+def main(gene_ids_csv, gff_directory, output_csv, merged_gff, not_found_file):
+    # List all files in the directory
+    files = os.listdir(gff_directory)
+    
+    # Filter out only the .gff files
+    gff_files = [file for file in files if file.endswith('.gff')]
+    
+    # Create a set to keep track of processed GFF files
+    processed_files = set()
+
+    # get ids for query not hit by blast
+    with open(not_found_file, 'r') as nf:
+        not_found_list = nf.read().splitlines()
+
+    # Open the output CSV file for writing
+    with open(output_csv, 'w', newline='') as csvfile:
+        # Define column headers
+        fieldnames = ['ID', 'Gene', 'Annotation']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # Write column headers
+        writer.writeheader()
+        
+        # Open the merged GFF file for writing
+        with open(merged_gff, 'w') as merged_file:
+            # Loop through each .gff file
+            for gff_file in gff_files:
+                query_id = gff_file.split(".")[0]
+                human_gene = get_query_gene(query_id, gene_ids_csv)
+
+                gff_path = os.path.join(gff_directory, gff_file)
+                
+                # Initialize annotation variable
+                gff_annotation = "no overlap"
+                
+                # Check if the current GFF file has been processed
+                if gff_file not in processed_files:
+                    # Log message for each new GFF file
+                    print(f"processing: {gff_path}")
+                    processed_files.add(gff_file)  # Add the current GFF file to the set of processed files
+
+                    # Read the first line of the GFF file and use it as annotation
+                    with open(gff_path, 'r') as f:
+                        gff_annotation = f.readline().strip()
+                        if gff_annotation == "":
+                            gff_annotation = "no overlap"
+
+                # get annotations
+                if query_id in not_found_list:
+                    gff_annotation = "not found by blast"
+                # Write overlaping annotations to a merged gff
+                if gff_annotation != "not found by blast":
+                    merged_file.write(gff_annotation)
+                    merged_file.write("\n")
+                
+                # Write id, gene, and annotation to output csv with column headers
+                writer.writerow({'ID': query_id, 'Gene': human_gene, 'Annotation': gff_annotation})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process .gff files and gene IDs CSV.')
+    parser.add_argument('gene_ids_csv', type=str, help='Path to the CSV file containing gene IDs.')
+    parser.add_argument('gff_directory', type=str, help='Path to the directory containing .gff files.')
+    parser.add_argument('output_csv', type=str, help='Path to the output CSV file.')
+    parser.add_argument('merged_gff', type=str, help='Path to the merged GFF file.')
+    parser.add_argument('not_found', type=str, help='Path to not_found.txt')
+    args = parser.parse_args()
+
+    main(args.gene_ids_csv, args.gff_directory, args.output_csv, args.merged_gff, args.not_found)
