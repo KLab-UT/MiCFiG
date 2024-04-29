227f9266c29ad310dcdc07b1c8f4a9160b3022aa
David-Bean <david.w.bean98@gmail.com>
2024-04-29 11:19:39 -0600

    added scatterplot function

--- ident_histogram.r
+++ ident_histogram.r
@@ -22,44 +22,86 @@ df <- data.frame(genes,pident)
 # Density Plot For Percent Identity #
 #####################################
 
-densityplot <- ggplot(df) +
-  theme_classic() +
-  geom_density(
-    aes(x=pident,y=after_stat(density)),
-    color = "darkblue",
-    fill="lightblue",
-    linewidth = 1)+
+make_density <- function(){
+  densityplot <- ggplot(df) +
+    theme_classic() +
+    geom_density(
+      aes(x=pident,y=after_stat(density)),
+      color = "darkblue",
+      fill="lightblue",
+      linewidth = 1)+
+    
+    geom_boxplot(
+      aes(x=pident),
+      width=0.0005,
+      fill="#f8fbae",
+      position=position_nudge(y=-0.00025))+
+    
+    geom_vline(aes(xintercept=mean_ident), color = "black",linetype="dotted") +
+    geom_vline(aes(xintercept=max_ident), color = "red",linetype="dotted")+
+    
+    geom_text(aes(x=mean_ident - x_label_offset,y = y_label_offset, angle=label_angle,label= mean_label))+
+    geom_text(aes(x=max_ident + x_label_offset,y = y_label_offset, angle=label_angle,label= max_label))
   
-  geom_vline(aes(xintercept=mean_ident), color = "black",linetype="dotted") +
-  geom_vline(aes(xintercept=max_ident), color = "red",linetype="dotted")+
+  densityplot
+}
 
-  geom_text(aes(x=mean_ident - x_label_offset,y = y_label_offset, angle=label_angle,label= mean_label))+
-  geom_text(aes(x=max_ident + x_label_offset,y = y_label_offset, angle=label_angle,label= max_label))
-
-  
-densityplot
 
 ##################################
 # Histogram For Percent Identity #
 ##################################
 
-histplot <- ggplot(df) +
-  theme_classic() +
-  geom_histogram(
-    aes(x=pident),
-    binwidth=8,
-    color="darkblue",
-    fill="lightblue")+
+make_hist <- function(){
   
-  geom_vline(aes(xintercept=mean_ident), color = "black",linetype="dotted") +
-  geom_vline(aes(xintercept=max_ident), color = "red",linetype="dotted")+
+  histplot <- ggplot(df) +
+    theme_classic() +
+    geom_histogram(
+      aes(x=pident),
+      binwidth=8,
+      color="darkblue",
+      fill="lightblue")+
+    
+    geom_vline(aes(xintercept=mean_ident), color = "black",linetype="dotted") +
+    geom_vline(aes(xintercept=max_ident), color = "red",linetype="dotted")+
+    
+    geom_text(aes(x=mean_ident - x_label_offset,y = -20, angle=label_angle,label= mean_label))+
+    geom_text(aes(x=max_ident + x_label_offset,y = -20, angle=label_angle,label= max_label))+
+    
+    xlab("Percent Identity")+
+    ylab("Number of Hits")
   
-  geom_text(aes(x=mean_ident - x_label_offset,y = -20, angle=label_angle,label= mean_label))+
-  geom_text(aes(x=max_ident + x_label_offset,y = -20, angle=label_angle,label= max_label))+
-  
-  xlab("Percent Identity")+
-  ylab("Number of Hits")
+  histplot
+}
 
 
-histplot
+
+make_scatter <- function(){
+  
+  scatter <- ggplot(df,aes(x=pident,y=0))+
+
+    geom_point(position = position_jitter(width = 10),
+               aes(colour = cut(pident,c(-Inf,100,Inf))),
+               shape=1,
+               )+
+
+    xlab("Percent Identity")+
+    ylab("")+
+    
+    
+    scale_color_manual(name="Spicyness",
+                       values=c("(-Inf,100]"="cornflowerblue",
+                                "(100,Inf]" = "indianred1"),
+                       
+                       labels=c("0-100",">100"))+
+    
+    theme(axis.ticks.y=element_blank(),
+            axis.text.y=element_blank()
+          )
+  scatter
+    }
+
+#make_hist()
+#make_density()
+make_scatter()
+
 
