d0624a0529ebcc377fd13796fc48a1adec1bac3c
David-Bean <david.w.bean98@gmail.com>
2024-04-28 20:06:43 -0600

    Polished histogram/density plot R script
    Made it look poster presentable. Option for histogram or density plot.
    Dashed vertical lines show mean percent identiy and max percent identity

--- ident_histogram.r
+++ ident_histogram.r
@@ -2,33 +2,64 @@
 library("ggplot2")
 
 stats <- read.csv("identities.csv",header = FALSE)
-
 genes <- stats[1]
-
-pident
 pident <- as.numeric(unlist(stats[2]))
 
-hist_plot <-ggplot(stats,aes(x=pident)) +
-  geom_histogram(binwidth = 1, show.legend = F) +
-		 theme_classic() + 
-		 scale_color_manual(values = c("gray", "gray")) +  # Change right values to black to bring 
-		 scale_fill_manual(values = c("gray", "gray")) +   # shading to values greater than BamA concervancy back
-      	         labs(x = "Percent Identity", y = "Number of Hits") +
-		 theme(axis.text = element_text(size = 18),  
-                 axis.title = element_text(size = 20))
+mean_ident <- mean(pident)
+mean_label = paste("mean ",round(mean_ident),"%",sep = "")
+
+max_ident <- max(pident)
+max_label = paste("max ",round(max_ident),"%",sep="")
+x_label_offset = -30
+y_label_offset = -0.0005
+label_angle = 0 #90
+
+
+df <- data.frame(genes,pident)
+
+
+#####################################
+# Density Plot For Percent Identity #
+#####################################
+
+densityplot <- ggplot(df) +
+  theme_classic() +
+  geom_density(
+    aes(x=pident,y=after_stat(density)),
+    color = "darkblue",
+    fill="lightblue",
+    linewidth = 1)+
   
-  # Adding Vertical lines to histogram
-  #geom_vline(aes(xintercept=bamA_proportion), color = "black") +
-  #geom_vline(aes(xintercept=tamA_proportion), color = "black")+
-  #geom_vline(aes(xintercept=secY_proportion),color = "black") +http://127.0.0.1:43203/graphics/d34d13c5-dfed-4b50-af4f-0ba9aa7fb0e9.png
-  
-  
-  # Labeling vertical lines
-  
-  #geom_text(aes(x=bamA_proportion-label_h_offset,y = 100, angle = label_angle, label = "BamA: 0.9815")) +
-  #geom_text(aes(x=tamA_proportion-label_h_offset,y = 110, angle = label_angle, label = "TamA: 0.9889")) +
-  #geom_text(aes(x=secY_proportion-label_h_offset,y = 120, angle = label_angle, label = "SecY: 0.9991")) 
+  geom_vline(aes(xintercept=mean_ident), color = "black",linetype="dotted") +
+  geom_vline(aes(xintercept=max_ident), color = "red",linetype="dotted")+
+
+  geom_text(aes(x=mean_ident - x_label_offset,y = y_label_offset, angle=label_angle,label= mean_label))+
+  geom_text(aes(x=max_ident + x_label_offset,y = y_label_offset, angle=label_angle,label= max_label))
 
   
-hist_plot
+densityplot
+
+##################################
+# Histogram For Percent Identity #
+##################################
+
+histplot <- ggplot(df) +
+  theme_classic() +
+  geom_histogram(
+    aes(x=pident),
+    binwidth=8,
+    color="darkblue",
+    fill="lightblue")+
+  
+  geom_vline(aes(xintercept=mean_ident), color = "black",linetype="dotted") +
+  geom_vline(aes(xintercept=max_ident), color = "red",linetype="dotted")+
+  
+  geom_text(aes(x=mean_ident - x_label_offset,y = -20, angle=label_angle,label= mean_label))+
+  geom_text(aes(x=max_ident + x_label_offset,y = -20, angle=label_angle,label= max_label))+
+  
+  xlab("Percent Identity")+
+  ylab("Number of Hits")
+
+
+histplot
 
